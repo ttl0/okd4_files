1f62c5f03fb2414681a1f4d11e2448bcc1c510b1
ttl0 <monasteriosd@gmail.com>
2021-07-05 15:46:42 -0400

    Update setdomain.sh

--- setdomain.sh
+++ setdomain.sh
@@ -1,12 +1,12 @@
 echo "Setting cluster domain name to: $1.$2"
 
 # Replace dns references in named config
-sudo sed -i 's/okd.home.lab/'$1.$2'/' db.192.168.1
-sudo sed -i 's/home.lab/'$2'/' db.192.168.1
-sudo sed -i 's/home.lab/'$2'/' db.192.168.1
+sudo sed -i 's/okd.home.lab/'$1.$2'/' db.10.0.2
+sudo sed -i 's/home.lab/'$2'/' db.10.0.2
+sudo sed -i 's/home.lab/'$2'/' db.10.0.2
 sudo sed -i 's/okd.home.lab/'$1.$2'/' db.home.lab
 sudo sed -i 's/home.lab/'$2'/' db.home.lab
-sudo sed -i 's/home.lab/'$2'/' db.192.168.1
+sudo sed -i 's/home.lab/'$2'/' db.10.0.2
 sudo mv db.home.lab db.$2
 sudo sed -i 's/home.lab/'$2'/' named.conf.local
 
